0f4fb77551ea362ebe0ec99f03ba9cdc203bafcd
disha-2288 <dishamehta280306@gmail.com>
2025-01-22 18:58:46 +0530

    Add files via upload
    lab 1 part B

--- 1.c
+++ 1.c
@@ -1,24 +1,13 @@
 #include <stdio.h>
 void main(){
-    char name[100];
-    int rollnumber;
-    int marks;
-    int sum=0;
-    float avg=0;
-        printf("Enter your name: ");
-        scanf("%s", name);
-        printf("Enter your roll number\n");
-        scanf("%d", &rollnumber);
-        for( int i=1;i<=4;i++){
-            printf("Enter marks of subject %d ",i);
-            scanf("%d", &marks);
-            sum=sum+marks;
-        }
-        printf("Your name is %s\n", name);
-        printf("Your roll number is %d\n", rollnumber);
-        printf(" total score %d/400\n",sum);
-        avg=(float)sum/4;
-        printf("Your percentage are: %f\n", avg);
-
-  
+    int a,b;
+    printf("Enter the value of a");
+    scanf("%d",&a);
+    printf("Enter the value of b");
+    scanf("%d",&b);
+    a=a-b;
+    b=a+b;
+   a=b-a;
+    printf("%d\n",a);
+    printf("%d",b);
 }
--- 2.c
+++ 2.c
@@ -1,48 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h>
+
 void main(){
-    int p=0;
-    int r=0;
-    int t=0;
-    float si=0;
-    float ci=0;
-    int a;
-    int n=0;
-    printf("Enter 1 for simple interest and 2 for compound interest: ");
-    scanf("%d",&a);
-    if(a==1){
-        printf("Enter principal:");
-   scanf("%d",&p);
-
-   printf("Enter rate:");
-   scanf("%d",&r);
-   
-   printf("Enter time:");
-   scanf("%d",&t);
-   
-   si=(p*r*t)/100;
-   
-   
-   printf("Simple Interest is: %f",si);
-
-    }
-    else if(a==2){
-        printf("Enter principal:");
-   scanf("%d",&p);
-   
-   printf("Enter rate:");
-   scanf("%d",&r);
-   
-   printf("Enter time:");
-   scanf("%d",&t);
-   printf("Enter n");
-   scanf("%d",&n);
-
-   
-   ci= p*1;
-   
-   printf("Compound Interest is: %f",ci);
-   
-   
-
-}
+    int a,b,c,s;
+    int area=0;
+    printf("Enter the value of a,b,c");
+    scanf("%d %d %d",&a,&b,&c);
+    s=(a+b+c)/2;
+    area= sqrt(s*(s-a)*(s-b)*(s-c));
+    printf("Area of given triangle is %d",area);
 }
